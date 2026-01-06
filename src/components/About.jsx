30e1f741f6fea2cf4c5b3420f2992ead648f33c1
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-01-06 18:27:20 +0530

    the code has been made responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
       <div className='flex mt-5 flex-col gap-4 text-white'>
         <h2 id="about" className="font-bold text-2xl w-full text-center justify-center">About</h2>
-        <div className='h-20 place-items-center px-10'>
-          <p className='font-bold w-1/2'>I am a Front-End Developer with a strong focus on building clean, responsive, and user-friendly web experiences. I’ve completed my Computer Science Engineering degree and enjoy crafting interfaces that blend creativity with performance.</p>
+        <div className='flex justify-center items-center px-10'>
+          <p className='font-bold w-1/2 text-center'>I am a Front-End Developer with a strong focus on building clean, responsive, and user-friendly web experiences. I’ve completed my Computer Science Engineering degree and enjoy crafting interfaces that blend creativity with performance.</p>
         </div>
-        <div className='h-20 place-items-center px-10'>
-          <p className='font-bold w-1/2'>I am passionate about modern web technologies, constantly exploring new tools, and turning ideas into polished digital products. Always eager to learn, grow, and collaborate with others to build meaningful projects.</p>
+        <div className='flex justify-center items-center px-10'>
+          <p className='font-bold w-1/2 text-center'>I am passionate about modern web technologies, constantly exploring new tools, and turning ideas into polished digital products. Always eager to learn, grow, and collaborate with others to build meaningful projects.</p>
         </div>
       </div>
   )
