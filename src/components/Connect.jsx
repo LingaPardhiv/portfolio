a8df76dfa5a00ac12d8c6c0e59f03f8ba40cbc1c
Linga Pardhiv <kogantilingapardhiv@gmail.com>
2026-01-08 17:48:19 +0530

    the code has been made responsive

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -4,7 +4,7 @@ const Connect = () => {
   return (
     <div>
       <div className="flex justify-center items-center text-white mt-6">
-        <div className='h-55 w-170 bg-zinc-900 opacity-70 rounded-2xl p-5 mt-5 gap-2 flex flex-row'>
+        <div className='md:h-55 w-170 bg-zinc-900 opacity-70 rounded-2xl p-5 mt-5 gap-2 flex flex-row'>
             <div id='contact'>
             <div className='ml-2 mt-2 font-bold text-2xl'>Let's Connect</div>
             <div className='ml-2 mt-2'>Have an idea or want to collaborate?</div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,18 +6,18 @@ const Projects = () => {
       <div className='mt-6 text-white'>
       <h2 id="projects" className='font-bold text-2xl w-full text-center justify-center'>Projects</h2>
       <div className="flex justify-center flex-col items-center">
-        <div className=' w-170 bg-zinc-900 rounded-2xl p-5 mt-5 gap-2'>
+        <div className=' md:w-170 w-sm bg-zinc-900 rounded-2xl p-5 mt-5 gap-2'>
           <div className='ml-2 font-bold'>BLOCKCHAIN-BASED DIGITAL IDENTITY VERIFICATION SYSTEM FOR E-COMMERCE</div>
           <div className='p-2'> &#8226; Designed and developed a blockchain-based digital identity verification system for e-commerce, enhancing user security, privacy, and control through smart contracts and decentralized storage.</div>
           <div className='p-1'> &#8226; Implemented cryptographic protocols and machine learning tools (Python, Solidity, scikit-learn, pandas, NumPy) to automate fraud detection, eliminate third-party data reliance, and build trust in online transactions.</div>
         </div>
-        <div className=' w-170 bg-zinc-900 rounded-2xl p-5 mt-5 gap-2'>
+        <div className=' md:w-170 w-sm bg-zinc-900 rounded-2xl p-5 mt-5 gap-2'>
           <div className='ml-2 font-bold'>LOAN ELIGIBILITY PREDICTION: MACHINE LEARNING APPROACH FOR FRAUDULENT LOAN ANALYSIS</div>
           <div className='m-2'> &#8226; Developed a machine learning-based system to predict loan eligibility and detect fraudulent applications using the SYL Bank dataset.</div>
           <div className='m-2'> &#8226; Automated risk assessment with algorithms like Random Forest and KNN, improving accuracy and efficiency in loan processing.</div>
           <div className='m-2'> &#8226; Utilized Python, scikit-learn, pandas, and NumPy for data preprocessing, model training, and performance evaluation.</div>
         </div>
-        <div className=' w-170 bg-zinc-900 rounded-2xl p-5 mt-5'>
+        <div className=' md:w-170 w-sm bg-zinc-900 rounded-2xl p-5 mt-5'>
           <div className='ml-2 font-bold'>COMMUNITY TUTORS: A NEW METHODOLOGY OF IMPLEMENTATION FOR FINDING NEARBY TUTORS</div>
           <div className='m-2'> &#8226; Developed Community Tutors, a website built with HTML, CSS, and JavaScript to connect students with nearby tutors or tuition centers, addressing learning gaps caused by the pandemic.</div>
           <div className='m-2'> &#8226; Implemented detailed tutor profiles, scheduling tools, and transparent fee information to ensure convenient, high-quality local educational support.</div>
